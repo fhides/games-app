284143bb66ce6588d6eb6e7797488b0ccbb0e967
fhidesjoseph <fhidesjoseph@gmail.com>
2022-09-09 22:59:07 +0300

    commit

--- app/controllers/application _controller.rb
+++ app/controllers/application _controller.rb
@@ -1,6 +1,25 @@
 class ApplicationController < Sinatra::Base
- 
- get '/' do 
-  '<h1>games today <h1>'
+  set :default_content_type, "application/json"
+
+ get '/countries' do
+
   end
-end
+
+   get '/countries' do 
+    countries = Country.all
+    countries.to_json
+
+   end
+
+   get '/leagues' do 
+    leagues = League.all
+    leagues.to_json
+
+   end
+
+   get '/games' do 
+    games = Game.all
+    games.to_json
+
+   end
+  end
